bef94455df5e224c67281d8766694b8ee082e41c
Douglas Kaiser <douglastkaiser@gmail.com>
2019-06-16 10:01:37 -0400

    Better plotting.

--- run_strategies.py
+++ run_strategies.py
@@ -8,34 +8,27 @@ def run_some_strategies(initial_deposit, daily_investment, close_prices):
     print("Initial Deposit: " + str(initial_deposit))
     print("Daily Investment: " + str(daily_investment))
 
-    fig = plt.figure()  # an empty figure with no axes
-    fig.suptitle('No axes on this figure')  # Add a title so we know which it is
-    x = np.linspace(0, 2, len(close_prices))
-
-    # plt.plot(x, close_prices, label='Closing Prices')
-    plt.xlabel('x label')
-    plt.ylabel('y label')
-    plt.title("Simple Plot")
-    # plt.show()
-
     print("\n -- Strat = NONE --")
-    strategy_no_investment_trading_day = run_strat_over_time(strategy_no_investment, close_prices, daily_investment, initial_deposit)
+    strategy_no_investment_trading_day = run_strat_over_full_time(strategy_no_investment, close_prices, daily_investment, initial_deposit)
     strategy_no_investment_trading_day.printer()
 
     print("\n -- Strat = ASAP --")
-    strategy_asap_investment_trading_day = run_strat_over_time(strategy_asap_investment, close_prices, daily_investment, initial_deposit)
+    strategy_asap_investment_trading_day = run_strat_over_full_time(strategy_asap_investment, close_prices, daily_investment, initial_deposit)
     strategy_asap_investment_trading_day.printer()
 
     print("\n -- Strat = SINUSOID --")
-    strategy_sinusoid_investment_trading_day = run_strat_over_time(strategy_sinusoid_investment, close_prices, daily_investment, initial_deposit)
+    strategy_sinusoid_investment_trading_day = run_strat_over_full_time(strategy_sinusoid_investment, close_prices, daily_investment, initial_deposit)
     strategy_sinusoid_investment_trading_day.printer()
 
     print("\n -- Strat = 100pt MAF --")
-    strategy_maf_investment_trading_day = run_strat_over_time(strategy_maf_investment, close_prices, daily_investment, initial_deposit)
+    strategy_maf_investment_trading_day = run_strat_over_full_time(strategy_maf_investment, close_prices, daily_investment, initial_deposit)
     strategy_maf_investment_trading_day.printer()
 
 
-    ## Print it
+    ## Print full values
+    fig = plt.figure()  # an empty figure with no axes
+    x = np.linspace(1, len(close_prices), len(close_prices))
+
     portfolio_value_history = strategy_no_investment_trading_day.portfolio_value_history()
     plt.plot(x, portfolio_value_history, label='No Investment Strat')
 
@@ -48,10 +41,14 @@ def run_some_strategies(initial_deposit, daily_investment, close_prices):
     portfolio_value_history = strategy_maf_investment_trading_day.portfolio_value_history()
     plt.plot(x, portfolio_value_history, label='MAF Strat')
 
+    plt.xlabel('Trading Days')
+    plt.ylabel('Value ($)')
+    plt.title("Portfolio Values for Different Strategies")
     plt.legend()
     plt.show()
 
-def run_strat_over_time(strat, close_prices, daily_investment, initial_deposit):
+
+def run_strat_over_full_time(strat, close_prices, daily_investment, initial_deposit):
 
     trading_day = TradingDay([initial_deposit], [0], [close_prices[0]])
 
